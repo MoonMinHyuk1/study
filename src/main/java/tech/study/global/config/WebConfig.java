2bc04a8b4c2aaffc3dcc19826dcb6b1fd982bc70
MoonMinHyuk1 <moon981018@gmail.com>
2023-07-04 12:54:41 +0900

    chore: project setting

--- src/main/java/tech/study/global/config/WebConfig.java
+++ src/main/java/tech/study/global/config/WebConfig.java
@@ -2,7 +2,9 @@ package tech.study.global.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import tech.study.global.resolver.AuthArgumentResolver;
 
@@ -17,5 +19,23 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(authArgumentResolver);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "http://localhost:8080",
+                        "https://teacking.check"
+                )
+                .allowedMethods(
+                        HttpMethod.GET.name(),
+                        HttpMethod.POST.name(),
+                        HttpMethod.PUT.name(),
+                        HttpMethod.DELETE.name(),
+                        HttpMethod.HEAD.name(),
+                        HttpMethod.PATCH.name()
+                )
+                .allowCredentials(true);
     }
 }
